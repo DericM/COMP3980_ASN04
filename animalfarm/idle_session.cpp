4b38880eaacd86d0e892b1538b061c65ac44a83f
DericM <dmccadden@bengaled.com>
2016-11-28 09:58:10 -0800

    moved port connection functions to idle_session

---
+++ animalfarm/idle_session.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include <stdexcept>
+#include "globalvar.h"
+
+
+/*
+* Open the comm port.
+*/
+void ipc_open_port(HWND& hWnd, LPCWSTR& lpszCommName) {
+	LOGMESSAGE(L"Entering: idle_open_port()\n");
+
+	COMMCONFIG cc;
+
+	//set comm settings
+	cc.dwSize = sizeof(COMMCONFIG);
+	cc.wVersion = 0x100;
+	GetCommConfig(GlobalVar::g_hComm, &cc, &cc.dwSize);
+	if (!CommConfigDialog(lpszCommName, hWnd, &cc)) {
+		throw std::runtime_error("Failed to configure Comm Settings");
+	}
+
+	//open comm port
+	GlobalVar::g_hComm = CreateFile(
+		lpszCommName,
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_FLAG_OVERLAPPED,
+		NULL
+	);
+
+	//check for failure
+	if (GlobalVar::g_hComm == INVALID_HANDLE_VALUE) {
+		throw std::runtime_error("Failed to open Comm Port");
+	}
+
+	SetCommState(GlobalVar::g_hComm, &cc.dcb);
+}
+
+
+
+/*
+* close port.
+*/
+void ipc_close_port() {
+	LOGMESSAGE(L"Entering: idle_close_port()\n");
+
+	//CloseHandle(osReader.hEvent);
+	CloseHandle(GlobalVar::g_hComm);
+}
+
+

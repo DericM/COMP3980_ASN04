1b0165a01f0e6077c4980d324172961bd7717180
DericM <dmccadden@bengaled.com>
2016-11-29 20:46:15 -0800

    updated recieve

--- animalfarm/receive.cpp
+++ animalfarm/receive.cpp
@@ -7,10 +7,11 @@
 
 bool ipc_recieve_ack() {
 	char target = 0x06;
+	DWORD toReadSize = 1;
 	char readChar[1];
 	int timeout = 500;//////////////////xxxxxxxxxxxxxxxx//////////////////////////
 
-	if (ipc_read_from_port(readChar, target, timeout)) {
+	if (ipc_read_from_port(readChar, toReadSize, target, timeout)) {
 		LOGMESSAGE(L"Successfuly recieved: " + target);
 		GlobalVar::g_bWaitACK = FALSE;
 		SetEvent(GlobalVar::g_hAckEvent);
@@ -24,9 +25,10 @@ bool ipc_recieve_ack() {
 
 bool ipc_recieve_enq(int timeout) {
 	char target = 0x05;
+	DWORD toReadSize = 1;
 	char readChar[1];
 
-	if (ipc_read_from_port(readChar, target, timeout)) {
+	if (ipc_read_from_port(readChar, toReadSize, target, timeout)) {
 		LOGMESSAGE(L"Successfuly recieved: " + target);
 		GlobalVar::g_bWaitENQ = FALSE;
 		SetEvent(GlobalVar::g_hEnqEvent);
@@ -40,10 +42,11 @@ bool ipc_recieve_enq(int timeout) {
 
 bool ipc_recieve_snq() {
 	char target = 0x16;
+	DWORD toReadSize = 1;
 	char readChar[1];
 	char timeout = 500;/////////////////////////////////////////////////////////
 
-	if (ipc_read_from_port(readChar, target, timeout)) {
+	if (ipc_read_from_port(readChar, toReadSize, target, timeout)) {
 		LOGMESSAGE(L"Successfuly recieved: " + target);
 		//GlobalVar::g_bWaitENQ = FALSE;
 		SetEvent(GlobalVar::g_hRXSynEvent);
@@ -57,10 +60,11 @@ bool ipc_recieve_snq() {
 
 bool ipc_recieve_packet() {
 	char target = NULL;
-	char readChar[1027];
+	DWORD toReadSize = 1026;
+	char readChar[1026];
 	int timeout = 500;
 
-	if (ipc_read_from_port(readChar, target, timeout)) {
+	if (ipc_read_from_port(readChar, toReadSize, target, timeout)) {
 		LOGMESSAGE(L"Successfuly recieved: packet");
 		//GlobalVar::g_bWaitENQ = FALSE;
 		//SetEvent(GlobalVar::g_hEnqEvent);
@@ -78,7 +82,7 @@ bool ipc_recieve_packet() {
 
 
 
-bool ipc_read_from_port(char readChar[], char target, int timeout) {
+bool ipc_read_from_port(char readChar[], DWORD toReadSize, char target, int timeout) {
 	HANDLE& hComm = GlobalVar::g_hComm;
 	DWORD dwRes;
 	OVERLAPPED osReader = { 0 };
@@ -94,7 +98,7 @@ bool ipc_read_from_port(char readChar[], char target, int timeout) {
 
 	if (!fWaitingOnRead) {
 		// Issue read operation.
-		if (!ReadFile(hComm, &readChar, sizeof(readChar), &eventRet, &osReader)) {
+		if (!ReadFile(hComm, readChar, toReadSize, &eventRet, &osReader)) {
 			if (GetLastError() != ERROR_IO_PENDING) {
 				LOGMESSAGE(L"Error reading from port. ");
 			}
@@ -123,7 +127,7 @@ bool ipc_read_from_port(char readChar[], char target, int timeout) {
 				return TRUE;
 			}
 			else {
-				//ack not received
+				//target not received
 			}
 		}
 		//  Reset flag so that another opertion can be issued.
--- animalfarm/receive.h
+++ animalfarm/receive.h
@@ -5,4 +5,4 @@ bool ipc_recieve_enq(int timeout);
 bool ipc_recieve_snq();
 bool ipc_recieve_packet();
 
-bool ipc_read_from_port(char readChar[], char target, int timeout);
+bool ipc_read_from_port(char readChar[], DWORD toReadSize, char target, int timeout);

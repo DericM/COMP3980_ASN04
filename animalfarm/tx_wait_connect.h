13123b10a02603643f0e7ae329ed2cb5692e4b99
Jamie Lee <RealJamieLee@gmail.com>
2016-12-03 14:33:55 -0800

    Fix a compile error.

--- animalfarm/tx_wait_connect.h
+++ animalfarm/tx_wait_connect.h
@@ -4,5 +4,5 @@
 
 BOOL txwc_setup(int &enqCounter, const std::wstring& fileName);
 DWORD WINAPI txwc_receive_ack(LPVOID);
-DWORD WINAPI tx_wait_ack(LPVOID);
+DWORD WINAPI txwc_receive_ack_event(LPVOID);
 void HandleReceivedAck();

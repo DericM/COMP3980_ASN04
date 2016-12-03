7374aaf66df9f30159f72e7c4174f686c6346a66
DericM <dmccadden@bengaled.com>
2016-12-03 15:17:02 -0800

    added transmission fail counter added setup state

--- animalfarm/tx_send_data.cpp
+++ animalfarm/tx_send_data.cpp
@@ -2,26 +2,35 @@
 #include <stdexcept>
 #include "GlobalVar.h"
 #include "send.h"
+#include "tx_wait_ack.h"
+#include "tx_send_data.h"
 
 
+#define TX_FAIL_COUNTER 3
 
-/*
-SP Set Up
-	Take in Frame
-	Go to SP Write
-	If Write packet to port fails
-		go back to IDLE Set Up
-	go to Wait For Ack WFA Wait
-*/
-void txsd_send(char frame[1027]) {
+bool txsd_setup(char frame[1027]) {
+	int transmission_attempts = 0;
+	while (transmission_attempts < TX_FAIL_COUNTER) {
+		if (txsd_send(frame)) {
+			return true;//go get next packet
+		}
+		transmission_attempts++;
+	}
+	return false;//return to idle
+}
+
+
+bool txsd_send(char frame[1027]) {
 	LOGMESSAGE(L"\nEntering: txsd_send() - ");
 
 	//send packet
 	if (!ipc_send_packet(frame)) {
-		return;//return to idle.
+		return false;//resend packet
 	}
-
 	//goto wait for ack.
-	
+	if (!txwa_receive_ack()) {
+		return false;//resend packet
+	}
+	return true;
 }
 

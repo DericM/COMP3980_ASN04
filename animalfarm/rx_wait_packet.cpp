6f6a549e5a63379b1751b46f18f101fe825c0b3d
DericM <dmccadden@bengaled.com>
2016-12-04 11:41:44 -0800

    cleaned wait_packet

--- animalfarm/rx_wait_packet.cpp
+++ animalfarm/rx_wait_packet.cpp
@@ -10,17 +10,6 @@
 #include <memory>
 #include <math.h>
 
-/*
-struct SParams
-{
-	HWND hWnd;
-	HANDLE hcomm;
-	int timer;
-	OVERLAPPED reader;
-};
-SParams sParam;
-*/
-
 struct SynParams
 {
 	HWND hWnd;
@@ -35,21 +24,6 @@ BOOL rxwp_setUp() {
 	LOGMESSAGE(L"\nEntering: rxwp_setUP\n");
 	SYN_TIMER = (ceil(8216 / GlobalVar::g_cc.dcb.BaudRate * 1000) * 3);
 
-
-	/*
-	OVERLAPPED reader = { 0 };
-	reader.hEvent = CreateEvent(
-		NULL,               // default security attributes
-		TRUE,               // manual-reset event
-		FALSE,              // initial state is nonsignaled
-		NULL    // object name
-		);
-
-	if (reader.hEvent == NULL) {
-		throw std::runtime_error("Failed to create event");
-	}
-	*/
-
 	GlobalVar::g_hRXSynEvent = CreateEvent(
 		NULL,               // default security attributes
 		TRUE,               // manual-reset event
@@ -57,11 +31,6 @@ BOOL rxwp_setUp() {
 		NULL    // object name
 		);
 
-	
-	//sParam.hcomm = GlobalVar::g_hComm;
-	//sParam.reader = reader;
-	//sParam.timer = SYN_TIMER;
-	
 
 	synParam.timer = SYN_TIMER;
 
@@ -84,78 +53,6 @@ DWORD WINAPI rx_read_for_syn(LPVOID pData_)
 	else {
 		//success
 	}
-	
-	/*
-	bool bWaitSyn = true;
-	while (bWaitSyn)
-	{
-		//LOGMESSAGE(L"Entered READ_FOR_SYN \n");
-		char readChar;
-		BOOL fWaitingOnRead = false;
-		DWORD eventRet;
-
-		if (sParam.reader.hEvent == NULL) {
-			//reader is null
-			LOGMESSAGE(L"Reader Event is NULL");
-			return false;
-		}
-		if (!fWaitingOnRead) {
-			// Issue read operation.
-			if (!ReadFile(sParam.hcomm, &readChar, 1, &eventRet, &sParam.reader)) {
-				if (GetLastError() != ERROR_IO_PENDING) {
-				}
-				else {
-					fWaitingOnRead = TRUE;
-				}
-
-			}
-			else {
-				// read completed immediately
-				if (readChar == 0x16) {//SYN
-					LOGMESSAGE(L"Received SYN.\n");
-					bWaitSyn = false;
-					HandleReceivedSYN();
-				}
-			}
-		}
-
-		if (fWaitingOnRead) {
-			eventRet = WaitForSingleObject(sParam.reader.hEvent, sParam.timer);
-
-			switch (eventRet) {
-			case WAIT_OBJECT_0:
-				if (!GetOverlappedResult(sParam.hcomm, &sParam.reader, &eventRet, FALSE)) {
-					//do something here
-				}
-				else {
-					// Read completed successfully.
-					if (readChar == 0x16) {//SYN
-						LOGMESSAGE(L"Received SYN. /n");
-						bWaitSyn = false;
-						HandleReceivedSYN();
-					}
-					else {
-						LOGMESSAGE(L"NON SYN CHARACTER RECEIVED");
-						//SYN not received
-					}
-				}
-				//  Reset flag so that another opertion can be issued.
-				fWaitingOnRead = FALSE;
-				break;
-			case WAIT_TIMEOUT:
-				// Operation isn't complete yet. fWaitingOnRead flag isn't
-				// changed since I'll loop back around, and I don't want
-				// to issue another read until the first one finishes.
-				//
-				// This is a good time to do some background work.
-				LOGMESSAGE(L"Entered READ_FOR_SYN TIMED OUT\n");
-				break;
-			}
-		}
-
-		ResetEvent(sParam.reader.hEvent);
-	}
-	*/
 
 	return 0;
 }

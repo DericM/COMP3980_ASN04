18e6e6267aa8cbe3510bc1f3690521abfeac6763
Deric Mccadden <dmccadden@bengaled.com>
2016-12-06 15:25:27 -0800

    Revamped receive

--- animalfarm/receive.cpp
+++ animalfarm/receive.cpp
@@ -4,189 +4,51 @@
 #include "packetDefine.h"
 #include "testingfunctions.h"
 
-struct ReceiveParams
-{
-	DWORD    timeout;
-	char   target;
-	DWORD  toReadSize;
-	char * readChar;
-};
-ReceiveParams recieveParam;
-
-int TERMINATE_THREAD_TIMEOUT2 = 500;
-
-HANDLE receiveThread;
-HANDLE receiveDataEvent;
-HANDLE terminateThreadEvent;
-bool   f_runningThread;
-
-
 bool ipc_recieve_ack(DWORD timeout) {
-	
-	createEvents();
 
 	char readChar[1];
-	recieveParam.timeout    = timeout;
-	recieveParam.toReadSize = 1;
-	recieveParam.target     = 0x06;
-	recieveParam.readChar   = readChar;
+	DWORD toReadSize = 1;
+	char target = 0x06;
 
-
-	receiveThread = CreateThread(NULL, 0, recieve_thread, NULL, 0, 0);
-	if (receiveThread)
-		CloseHandle(receiveThread);
-
-	DWORD dwRes = WaitForSingleObject(receiveDataEvent, timeout);
-	ResetEvent(receiveDataEvent);
-
-	switch (dwRes)
-	{
-	case WAIT_OBJECT_0:
+	if (ipc_read_from_port(readChar, toReadSize, target, timeout)) {
 		LOGMESSAGE(L"Received ACK----Timestamp:" << generateTimestamp() << "\n");
 		return true;
-	case WAIT_TIMEOUT:
-		LOGMESSAGE(L"Timeout ACK-----Timestamp:" << generateTimestamp() << L"-----Timeout:" << timeout << "\n");
-		ipc_terminate_read_thread();
-		return false;
-	default:
-		LOGMESSAGE("Something bad");
-		break;
 	}
+	LOGMESSAGE(L"Timeout ACK-----Timestamp:" << generateTimestamp() << L"-----Timeout:" << timeout << "\n");
 	return false;
 }
 
 bool ipc_recieve_enq(DWORD timeout) {
 
-	createEvents();
-
 	char readChar[1];
-	recieveParam.timeout    = timeout;
-	recieveParam.toReadSize = 1;
-	recieveParam.target     = 0x05;
-	recieveParam.readChar   = readChar;
+	DWORD toReadSize = 1;
+	char target = 0x05;
 
-	receiveThread = CreateThread(NULL, 0, recieve_thread, NULL, 0, 0);
-	if (receiveThread)
-		CloseHandle(receiveThread);
-
-	DWORD dwRes = WaitForSingleObject(receiveDataEvent, timeout);
-	ResetEvent(receiveDataEvent);
-
-	switch (dwRes)
-	{
-	case WAIT_OBJECT_0:
+	if (ipc_read_from_port(readChar, toReadSize, target, timeout)) {
 		LOGMESSAGE(L"Received ENQ----Timestamp:" << generateTimestamp() << "\n");
 		return true;
-	case WAIT_TIMEOUT:
-		LOGMESSAGE(L"Timeout ENQ-----Timestamp:" << generateTimestamp() << L"-----Timeout:" << timeout << "\n");
-		ipc_terminate_read_thread();
-		return false;
-	default:
-		LOGMESSAGE("Something bad");
-		break;
 	}
+	LOGMESSAGE(L"Timeout ENQ-----Timestamp:" << generateTimestamp() << L"-----Timeout:" << timeout << "\n");
 	return false;
 }
 
-/*
-bool ipc_recieve_syn(DWORD timeout) {
-
-	createEvents();
-
-	char readChar[1];
-	recieveParam.timeout    = timeout;
-	recieveParam.toReadSize = 1;
-	recieveParam.target     = 0x16;
-	recieveParam.readChar   = readChar;
-
-
-	receiveThread = CreateThread(NULL, 0, recieve_thread, NULL, 0, 0);
-
-	DWORD dwRes = WaitForSingleObject(receiveDataEvent, timeout);
-	ResetEvent(receiveDataEvent);
-	long long ms = duration_cast<milliseconds>(
-		system_clock::now().time_since_epoch()
-		).count() - 1480980000000;
-	switch (dwRes)
-	{
-	case WAIT_OBJECT_0:
-		LOGMESSAGE(L"Received SYN----Timestamp:" << ms << "\n");
-		return true;
-	case WAIT_TIMEOUT:
-		LOGMESSAGE(L"Timeout SYN-----Timestamp:" << ms << "\n");
-		ipc_terminate_read_thread();
-		return false;
-	default:
-		LOGMESSAGE("Something bad");
-		break;
-	}
-	return false;
-}
-*/
 
 bool ipc_recieve_packet(char * readChar, DWORD timeout) {
 
-	createEvents();
+	DWORD toReadSize = HEADER_SIZE + DATA_SIZE + CRC_SIZE;
+	char target     = 0x16;
 
-	recieveParam.timeout    = timeout;
-	recieveParam.toReadSize = HEADER_SIZE + DATA_SIZE + CRC_SIZE;
-	recieveParam.target     = 0x16;
-	recieveParam.readChar   = readChar;
-
-
-	receiveThread = CreateThread(NULL, 0, recieve_thread, NULL, 0, 0);
-	if (receiveThread)
-		CloseHandle(receiveThread);
-
-	DWORD dwRes = WaitForSingleObject(receiveDataEvent, timeout);
-	ResetEvent(receiveDataEvent);
-
-	switch (dwRes)
-	{
-	case WAIT_OBJECT_0:
+	if (ipc_read_from_port(readChar, toReadSize, target, timeout)) {
 		LOGMESSAGE(L"Received PACKET-Timestamp:" << generateTimestamp() << "\n");
 		return true;
-	case WAIT_TIMEOUT:
-		LOGMESSAGE(L"Timeout PACKET--Timestamp:" << generateTimestamp() << "\n");
-		ipc_terminate_read_thread();
-		return false;
-	default:
-		LOGMESSAGE("Something bad");
-		break;
 	}
+	LOGMESSAGE(L"Timeout PACKET--Timestamp:" << generateTimestamp() << "\n");
 	return false;
 }
 
 
-bool createEvents() {
-	receiveDataEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-	if (receiveDataEvent == NULL) {
-		LOGMESSAGE(L"Failed to create hEvent. ");
-		return false;
-	}
-	terminateThreadEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-	if (terminateThreadEvent == NULL) {
-		LOGMESSAGE(L"Failed to create hEvent. ");
-		return false;
-	}
-	return true;
-}
 
-
-
-
-DWORD WINAPI recieve_thread(LPVOID na) {
-
-	ipc_read_from_port(recieveParam.readChar, 
-					   recieveParam.toReadSize, 
-					   recieveParam.target, 
-					   recieveParam.timeout);
-	return 0;
-}
-
-
-
-void ipc_read_from_port(char * readChar, DWORD toReadSize, char target, DWORD timeout) {
+bool ipc_read_from_port(char * readChar, DWORD toReadSize, char target, DWORD timeout) {
 	HANDLE& hComm = GlobalVar::g_hComm;
 
 	static BOOL fWaitingOnRead = FALSE;
@@ -197,100 +59,65 @@ void ipc_read_from_port(char * readChar, DWORD toReadSize, char target, DWORD ti
 	osReader.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 	if (osReader.hEvent == NULL) {
 		LOGMESSAGE(L"Failed to create hEvent. ");
-		return;
+		return false;
 	}
 
 	//DWORD readFileTimeout = static_cast<DWORD>(ceil(8.0 * toReadSize / GlobalVar::g_cc.dcb.BaudRate * 1000 ));
 
-	f_runningThread = true;
-	while (f_runningThread) {
-		//LOGMESSAGE(L"BEGIN==>");
-		if (!fWaitingOnRead) {
-			if (!ReadFile(hComm, readChar, toReadSize, &eventRet, &osReader)) {
-				if (GetLastError() != ERROR_IO_PENDING) {
-					LOGMESSAGE(L"Error reading from port." << GetLastError << " \n");
-				}
-				else {
-					//LOGMESSAGE(L"WAITING_TO_READ==>");
-					fWaitingOnRead = TRUE;
-				}
+	if (!ReadFile(hComm, readChar, toReadSize, &eventRet, &osReader)) {
+		if (GetLastError() != ERROR_IO_PENDING) {
+			LOGMESSAGE(L"Error reading from port." << GetLastError << " \n");
+		}
+		else {
+			//LOGMESSAGE(L"WAITING_TO_READ==>");
+			fWaitingOnRead = TRUE;
+		}
+	}
+	else {
+
+		if (target == NULL || readChar[0] == target) {
+			//LOGMESSAGE(L"GOT_TARGET1==>");
+			return true;
+		}
+		//LOGMESSAGE(L"GOT_NOTHING1==>");
+	}
+	if (fWaitingOnRead) {
+		DWORD dwRes = WaitForSingleObject(osReader.hEvent, timeout);
+		switch (dwRes)
+		{
+		case WAIT_OBJECT_0:
+			//LOGMESSAGE(L"WAIT_OBJECT_0==>");
+			if (!GetOverlappedResult(hComm, &osReader, &eventRet, FALSE)) {
+				LOGMESSAGE(L"!GetOverlappedResult()");
 			}
 			else {
-
 				if (target == NULL || readChar[0] == target) {
-					//LOGMESSAGE(L"GOT_TARGET1==>");
-					f_runningThread = false;
-					successfulyReceivedData = true;
-				}
-				//LOGMESSAGE(L"GOT_NOTHING1==>");
-			}
-		}
-		if (fWaitingOnRead) {
-			DWORD dwRes = WaitForSingleObject(osReader.hEvent, timeout);
-			switch (dwRes)
-			{
-			case WAIT_OBJECT_0:
-				//LOGMESSAGE(L"WAIT_OBJECT_0==>");
-				if (!GetOverlappedResult(hComm, &osReader, &eventRet, FALSE)) {
-					LOGMESSAGE(L"!GetOverlappedResult()");
+					//LOGMESSAGE(L"GOT_TARGET2==>");
+					return true;
 				}
 				else {
-					if (target == NULL || readChar[0] == target) {
-						LOGMESSAGE(L"GOT_TARGET2==>");
-						f_runningThread = false;
-						successfulyReceivedData = true;
-					}
-					else {
-						//LOGMESSAGE(L"GOT_NOTHING2==>");
-					}
+					//LOGMESSAGE(L"GOT_NOTHING2==>");
 				}
-				fWaitingOnRead = FALSE;
-				break;
-			case WAIT_TIMEOUT:
-				//LOGMESSAGE(L"WAIT_TIMEOUT==>" << target << std::endl);
-				break;
-
-			default:
-				LOGMESSAGE(L"DEFAULT==>\n");
-				break;
 			}
+			fWaitingOnRead = FALSE;
+			break;
+		case WAIT_TIMEOUT:
+			CancelIo(hComm);
+			//LOGMESSAGE(L"WAIT_TIMEOUT==>" << target << std::endl);
+			break;
 
-
+		default:
+			LOGMESSAGE(L"DEFAULT==>\n");
+			break;
 		}
-		ResetEvent(osReader.hEvent);
-		//LOGMESSAGE(L"END\n");
 	}
-	if (successfulyReceivedData) {
-		SetEvent(receiveDataEvent);
-	}
+	ResetEvent(osReader.hEvent);
+	return false;
 
-	SetEvent(terminateThreadEvent);
 }
 
 
 
-bool ipc_terminate_read_thread()
-{
-	f_runningThread = false;
-
-	DWORD dwRes = WaitForSingleObject(terminateThreadEvent, TERMINATE_THREAD_TIMEOUT2);
-	switch (dwRes)
-	{
-	case WAIT_OBJECT_0:
-		//CloseHandle(hThread);
-		return true;
-
-	case WAIT_TIMEOUT:
-		TerminateThread(receiveThread, 0);
-		//CloseHandle(hThread);
-		return false;
-
-	default:
-		TerminateThread(receiveThread, 0);
-		//CloseHandle(hThread);
-		return false;
-	}
-}
 
 
 
--- animalfarm/receive.h
+++ animalfarm/receive.h
@@ -4,7 +4,7 @@ bool ipc_recieve_ack(DWORD timeout);
 bool ipc_recieve_enq(DWORD timeout);
 bool ipc_recieve_syn(DWORD timeout);
 bool ipc_recieve_packet(char* readchar, DWORD timeout);
-void ipc_read_from_port(char* readChar, DWORD toReadSize, char target, DWORD timeout);
+bool ipc_read_from_port(char* readChar, DWORD toReadSize, char target, DWORD timeout);
 DWORD WINAPI recieve_thread(LPVOID na);
 bool ipc_terminate_read_thread();
 

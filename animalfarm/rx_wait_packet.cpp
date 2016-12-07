2ca85df590a4d24ea8bd50409054a4cfffa294db
Jamie Lee <RealJamieLee@gmail.com>
2016-12-06 22:30:10 -0800

    Make keep trying to read SYN

--- animalfarm/rx_wait_packet.cpp
+++ animalfarm/rx_wait_packet.cpp
@@ -4,21 +4,26 @@
 #include "rx_parse_packet.h"
 #include "receive.h"
 #include "packetDefine.h"
+#include "testingfunctions.h"
 
 
 bool rxwp_wait_for_packet() {
 	DWORD packetSize = HEADER_SIZE + DATA_SIZE + CRC_SIZE;
 	char packet[HEADER_SIZE + DATA_SIZE + CRC_SIZE] = { 0 };
+	DWORD SYN_TIMER = 500;
+	long long startTime = generateTimestamp();
+	DWORD PACKET_TIMER = static_cast<DWORD>(ceil(8.0 * packetSize / GlobalVar::g_cc.dcb.BaudRate * 1000) * 3);
+	PACKET_TIMER = 3000;
 
-	if (ipc_recieve_syn(500))
+	while (generateTimestamp() - startTime < PACKET_TIMER)
+	if (ipc_recieve_syn(SYN_TIMER))
 	{
-		DWORD timeout = static_cast<DWORD>(ceil(8.0 * packetSize / GlobalVar::g_cc.dcb.BaudRate * 1000) * 3);
-		timeout = 3000;
-		if (ipc_recieve_packet(packet, timeout)) {
+		if (ipc_recieve_packet(packet, PACKET_TIMER)) {
 			if (rxpp_parse_packet(packet)) {
 				return true;//packet recieved
 			}
 		}
+		break;
 	}
 
 	//DWORD timeout = static_cast<DWORD>(ceil(8.0 * packetSize / GlobalVar::g_cc.dcb.BaudRate * 1000) * 3);

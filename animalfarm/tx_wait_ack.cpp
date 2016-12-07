7205d3b3668c6c1cc826a16bcc1ee995ae630f80
Jamie Lee <RealJamieLee@gmail.com>
2016-12-07 00:42:35 -0800

    Change ACK_TIMER in txwa_receive_ack

--- animalfarm/tx_wait_ack.cpp
+++ animalfarm/tx_wait_ack.cpp
@@ -4,12 +4,16 @@
 #include "receive.h"
 #include "idle.h"
 #include "globalvar.h"
+#include "packetDefine.h"
 
 /*
 	Waits for ACK after sending a packet.
 */
 bool txwa_receive_ack() {
-	if (ipc_recieve_ack(GlobalVar::TXWA_ACK_TIMER)) {
+	DWORD packetSize = HEADER_SIZE + DATA_SIZE + CRC_SIZE;
+	DWORD ACK_TIMER = static_cast<DWORD>(ceil(8.0 * packetSize / GlobalVar::g_cc.dcb.BaudRate * 1000) * 3);
+	ACK_TIMER = 3000;
+	if (ipc_recieve_ack(ACK_TIMER)) {
 		return true;//success
 	}
 	return false;//failed to send packet

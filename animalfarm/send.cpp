4a629cafc741603e0d1ae8f4947cd87c25795210
Deric Mccadden <dmccadden@bengaled.com>
2016-12-06 19:00:12 -0800

    adjusted purgecomm

--- animalfarm/send.cpp
+++ animalfarm/send.cpp
@@ -44,7 +44,7 @@ bool ipc_send_data_to_port(const char* data, DWORD dwToWrite) {
 	DWORD dwRes;
 	bool fRes;
 	
-	PurgeComm(GlobalVar::g_hComm, PURGE_RXCLEAR);
+	PurgeComm(GlobalVar::g_hComm, PURGE_RXABORT | PURGE_TXABORT | PURGE_RXCLEAR | PURGE_TXCLEAR);
 
 	osWrite.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 	if (osWrite.hEvent == NULL) {

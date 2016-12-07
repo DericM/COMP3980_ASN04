5ad9584b211fbc147fc83849458388ec5d1346f0
Jamie Lee <RealJamieLee@gmail.com>
2016-12-07 02:07:02 -0800

    Change parsing packet logic.

--- animalfarm/rx_parse_packet.cpp
+++ animalfarm/rx_parse_packet.cpp
@@ -7,18 +7,18 @@
 bool rxpp_parse_packet(const char* packet) {
 	char dataBuffer[DATA_SIZE + 1];
 	uint16_t syndrome;
-	memcpy_s(dataBuffer, DATA_SIZE, packet + HEADER_SIZE, DATA_SIZE);
-	memcpy_s(&syndrome, CRC_SIZE, packet + HEADER_SIZE + DATA_SIZE, CRC_SIZE);
+	memcpy_s(dataBuffer, DATA_SIZE, packet, DATA_SIZE);
+	memcpy_s(&syndrome, CRC_SIZE, packet + DATA_SIZE, CRC_SIZE);
 	dataBuffer[DATA_SIZE] = '\0';
 	std::string strData(dataBuffer);
 	size_t lastChar = strData.find_first_of('x');
 	if (lastChar != std::string::npos)
 		strData = strData.substr(0, lastChar);
 	
-	std::string tempStr = "";
-	tempStr += strData.length();
-	tempStr += " : ";
-	tempStr += strData;
+	std::ostringstream stringStream;
+	stringStream << strData.length() << " : " << strData << std::endl;
+	std::string tempStr = stringStream.str();
+
 	SendMessageA(GlobalVar::g_hSendBox, EM_REPLACESEL, 0, (LPARAM)tempStr.c_str());
 
 	if (rxce_check_error(strData, syndrome)) {

3ba67710d09c4f6312b525883be3001b51fde0eb
Deric Mccadden <dmccadden@bengaled.com>
2016-12-07 00:02:58 -0800

    stuff

--- animalfarm/rx_wait_packet.cpp
+++ animalfarm/rx_wait_packet.cpp
@@ -9,8 +9,10 @@
 bool rxwp_wait_for_packet() {
 	
 	char packet[HEADER_SIZE + DATA_SIZE + CRC_SIZE];
+	
 
 	if (ipc_recieve_packet(packet, GlobalVar::T_RX)) {
+		packet[HEADER_SIZE + DATA_SIZE + CRC_SIZE] = '\0';
 		LOGMESSAGE(L"rawpacket:" << packet << "\n")
 		if (rxpp_parse_packet(packet)) {
 			return true;//packet recieved
